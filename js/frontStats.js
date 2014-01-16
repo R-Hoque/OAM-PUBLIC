9a13ec2d7a29ca6860cd7636c09f5c19b665cda5
rgwozdz <rgwozdz@spatialdev.com>
2014-01-16 13:13:13 -0800

    Pull correct quick stats activity counts.

--- js/frontStats.js
+++ js/frontStats.js
@@ -10,12 +10,19 @@ _SPDEV.FrontStats.init = function(){
 	  type: 'GET',
 	  async: false,
 	  success: function(data) {
-	    //console.log('got it!');
-	    quickStats = data;
-	    $('#agStats').html(quickStats[6].count);
-	    $('#transStats').html(quickStats[54].count);
-	    $('#edStats').html(quickStats[20].count);
-	    $('#healthStats').html(quickStats[29].count);
+
+	    $('#qsSector1Cnt').html(data[0].cnt);
+	    $('#qsSector1Name').html(data[0].name);
+	    
+	    $('#qsSector2Cnt').html(data[1].cnt);
+	    $('#qsSector2Name').html(data[1].name);
+	    
+	    $('#qsSector3Cnt').html(data[2].cnt);
+	    $('#qsSector3Name').html(data[2].name);
+	    
+	    $('#qsSector4Cnt').html(data[3].cnt);
+	    $('#qsSector4Name').html(data[3].name);
+
 	  },
 	  error: function(jqXHR, errorThrown) {
 	    console.log('error...');
--- php/getQuickStats.php
+++ php/getQuickStats.php
@@ -1,22 +1,12 @@
 <?php
 
-	/*ini_set('display_errors', 'On');
+	ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
-	require('db.inc');*/
-	
-	
-	$host = "";
-	$database = "";
-	$username = "";
-	$password = "";
-	
-	# DB connection string
-	$dbPostgres = pg_connect("host=$host port=5432 dbname=$database user=$username password=$password")
-	   or die("Could not connect");
-	
+	require('db.inc');
+
 	
 	# Build SQL SELECT statement and return the geometry as a GeoJSON element
-	$sql = "SELECT COUNT(project_ID), classification FROM activity_taxonomies GROUP BY classification ORDER BY classification ASC";
+	$sql = "SELECT COUNT(activity_id) AS cnt, classification AS name FROM activity_taxonomies WHERE taxonomy = 'Sector' GROUP BY classification ORDER BY cnt DESC LIMIT 4";
 
 	$result = pg_query($dbPostgres, $sql);
 
