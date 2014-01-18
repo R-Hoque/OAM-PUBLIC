01a5584a5b012ce7b0dca84179e491010175ea26
Grant McKenzie <grantdmckenzie@gmail.com>
2014-01-18 13:49:29 -0800

    loaded data in to the Sector Editor UI

---
+++ sector_editor/php/getSectors.php
@@ -0,0 +1,28 @@
+<?php
+
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+	require('../../php/db.inc');
+	
+	// Get the data
+	try {
+
+		$sql = "select * from pmt_taxonomies('15');";
+
+		// Prepare a query for execution
+		$result = pg_prepare($dbPostgres, "my_query", $sql);
+		
+		// Execute the prepared query.
+		$result = pg_execute($dbPostgres, "my_query", array());
+		
+		$rows = pg_fetch_all($result);
+
+		$r = json_decode($rows[0]['response'])->classifications;
+		echo json_encode($r, JSON_NUMERIC_CHECK);	
+			
+	} catch(Exception $e) {  
+	      die( print_r( $e->getMessage() ) );  
+	}
+	pg_close($dbPostgres);
+?>
+
