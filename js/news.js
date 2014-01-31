618489209934bd75a172b7dcd35ce96400f22fc4
abenrob <adamben.roberts@gmail.com>
2014-01-31 13:31:21 -0800

    news updates

--- js/news.js
+++ js/news.js
@@ -1,7 +1,7 @@
 _SPDEV.FrontNews = {};
 
-_SPDEV.FrontNews.init = function(){
-	
+_SPDEV.FrontNews.init = function(allstories){
+	allstories = allstories||false;
 	var newsContent = null;
 	var newsURL = "php/getNewsFiles.php";
 	$.ajax({
@@ -11,13 +11,24 @@ _SPDEV.FrontNews.init = function(){
 	  async: false,
 	  success: function(data) {
 	    newsContent = data.sort().reverse();
-			for (i in newsContent) {
+		for (i in newsContent) {
+			if ( allstories ) {
 				var storyi = newsContent[i].slice(3);
-	      if (storyi.slice(-13) != "template.html"){
-	  			$.get(storyi, function(data){ 
-	  				$('#qsnewsStories').append(data);
-	  			});
-	      }
+				if (storyi.slice(-13) != "template.html"){
+					$.get(storyi, function(data){ 
+						$('#qsnewsStories').append(data);
+					});
+				}
+			} else {
+				if ( i <=3 ){
+					var storyi = newsContent[i].slice(3);
+					if (storyi.slice(-13) != "template.html"){
+						$.get(storyi, function(data){ 
+							$('#qsnewsStories').append(data);
+						});
+					}
+				}
+			}
 		}
 	  },
 	  error: function(jqXHR, errorThrown) {
