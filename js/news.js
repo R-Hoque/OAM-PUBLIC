b1e44c5b1197857178c72853e1650d60d549b078
abenrob <adamben.roberts@gmail.com>
2014-02-07 11:25:08 -0800

    added in local linking for news stories

--- js/news.js
+++ js/news.js
@@ -1,6 +1,6 @@
 _SPDEV.FrontNews = {};
 
-_SPDEV.FrontNews.init = function(allstories){
+_SPDEV.FrontNews.init = function(allstories,filter){
 	allstories = allstories||false;
 	var newsContent = null;
 	var newsURL = "php/getNewsFiles.php";
@@ -15,16 +15,32 @@ _SPDEV.FrontNews.init = function(allstories){
 			if ( allstories ) {
 				var storyi = newsContent[i].slice(3);
 				if (storyi.slice(-13) != "template.html"){
-					$.get(storyi, function(data){ 
-						$('#qsnewsStories').append(data);
-					});
+					if ($.inArray('../content/news/'+filter,data) != -1){
+						console.log(filter,storyi);
+						if ('content/news/'+filter == storyi) {
+							$.get(storyi, function(story){
+								var linkedStory = $(story);
+								$(linkedStory.find('.article_source')[0]).attr('href','news.html?article='+storyi.slice(13));
+								$('#qsnewsStories').append(story);
+							});
+						}
+					} else {
+						$.get(storyi, function(story){ 
+							var linkedStory = $(story);
+							$(linkedStory.find('.article_source')[0]).attr('href','news.html?article='+storyi.slice(13));
+							$('#qsnewsStories').append(linkedStory);
+						});
+					}
+					
 				}
 			} else {
 				if ( i <=3 ){
 					var storyi = newsContent[i].slice(3);
 					if (storyi.slice(-13) != "template.html"){
-						$.get(storyi, function(data){ 
-							$('#qsnewsStories').append(data);
+						$.get(storyi, function(story){ 
+							var linkedStory = $(story);
+							$(linkedStory.find('.article_source')[0]).attr('href','news.html?article='+storyi.slice(13));
+							$('#qsnewsStories').append(story);
 						});
 					}
 				}
