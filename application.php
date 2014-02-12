1167d5279ce01218488c6c0f5ee89fec25303455
jharpster <jharpster@spatialdev.com>
2014-02-12 13:37:25 -0800

    minor text changes

--- application.php
+++ application.php
@@ -43,7 +43,7 @@
 			<nav class="header-nav right">
 				<ul class="clearfix">
 					<li id="locationButton" class="region-select dropdown"></li>
-					<li><a id='menu_associations' href="partnership.html">PARTNERSHIPS</a></li>
+					<li><a id='menu_associations' href="partnership.html">PARTNERSHIP</a></li>
 					<li><a id='menu_news' href="news.html">NEWS</a></li>
 					<!--attempting to move login to applicaiton page footer-->
 					<!--<li id="login" class="dropdown login"><a id="login_name" title="Login">LOGIN</a></li>-->
