680fca4cb5e4cca76759e020d1e190872d8078d7
rgwozdz <rgwozdz@spatialdev.com>
2014-03-17 12:27:55 -0700

    Sanitize handler.

--- php/getQuickStats.php
+++ php/getQuickStats.php
@@ -1,7 +1,6 @@
 <?php
 
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
+
 	require('db.inc');
 
 	
--- php/getTaxonomyClassifications.php
+++ php/getTaxonomyClassifications.php
@@ -1,15 +1,62 @@
 <?php
 
 	require('db.inc');
+	require('utils.inc');
+
 	require('translate.inc');
-	
-	$taxonomies = $_POST['taxonomyIds'];
-	$dataGroup = $_POST['dataGroupId'];
-	$countryIds = $_POST['countryIds'];
-	$language =  $_POST['language'];
-	
+
+	$taxonomies = null;
+	$dataGroup = null;
+	$countryIds = null;
+	$language =  null;
+
 	// Get the data
 	try {
+	
+		if (isset($_POST['dataGroupId'])) {
+	    	
+	    	$dataGroup = intval($_POST['dataGroupId']);
+	    	
+	    	// Validate that this is an integer
+			if(is_int($dataGroup) == false) {
+				throw new Exception('Bad Request', 400);
+			}
+		} else {
+			throw new Exception('Bad Request', 400);
+		}
+
+		if (isset($_POST['countryIds'])) {
+
+	    	$countryIds = $_POST['countryIds'];
+	    	
+	    	if(validateCommaDelimitedIntString($countryIds) == false) {
+	    		throw new Exception('Bad Request ', 400);
+	    	}
+
+		}
+
+		if (isset($_POST['language'])) {
+	    	
+	    	$language = strtolower($_POST['language']);
+
+	    	if($language == 'spanish' || $language == 'english') {}
+	    	else{
+	    		throw new Exception('Bad Request', 400);
+	    	}
+		} else {
+			throw new Exception('Bad Request ', 400);
+		}
+
+		if (isset($_POST['taxonomyIds'])) {
+
+	    	$taxonomies = $_POST['taxonomyIds'];
+	    	
+	    	if(validateCommaDelimitedIntString($taxonomies) == false) {
+	    		throw new Exception('Bad Request ', 400);
+	    	}
+
+		}
+
 
 		$sql = "select * from pmt_tax_inuse(". $dataGroup .", '". $taxonomies ."', '" . $countryIds . "');";
 
@@ -55,7 +102,8 @@
 		echo json_encode($data, JSON_NUMERIC_CHECK);	
 			
 	} catch(Exception $e) {  
-	      die( print_r( $e->getMessage() ) );  
+ 	    header('HTTP/1.1 ' . $e->getCode() . ' ' . $e->getMessage());
+ 	    die(); 
 	}
 	
 	pg_close($dbPostgres);
