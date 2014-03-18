c4efedbed451c45ef49e5a97d2fa9f39cb8e3f8b
rgwozdz <rgwozdz@spatialdev.com>
2014-03-18 11:53:50 -0700

    Remove ref to session inc.

--- sector_editor.php
+++ sector_editor.php
@@ -4,7 +4,20 @@
   @client = World Bank Open Aid Partnership
   @functionality = Main file for the Sector Editor (HTML/PHP)
   =========================================================== -->
- <?php require("php/session.inc"); ?>
+ <?php 
+
+ 	session_set_cookie_params ( 3600, null, null, null, true);
+	session_start();
+	if (isset($_SESSION['oamuser'])) {
+		require_once 'php/user.inc';
+		$oamuser = unserialize($_SESSION['oamuser']);
+		echo "<script>var _oamuser='".json_encode($oamuser)."';</script>";
+	} else {
+	    //if(strpos($_SERVER['PHP_SELF'], "application.php") == 0)
+		header('Location: index.html');
+	}
+
+ ?>
   <!DOCTYPE html>
   <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
   <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
